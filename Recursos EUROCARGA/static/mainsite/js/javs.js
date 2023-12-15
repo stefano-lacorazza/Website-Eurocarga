9cad0ce3699dff47ae5d8b42cbe7f44f55bbc4d1
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-12-15 09:46:10 -0500

    Added english language

---
+++ Recursos EUROCARGA/static/mainsite/js/javs.js
@@ -0,0 +1,48 @@
+function expanded(card) {
+    card.classList.toggle('profile--expanded');
+
+    // If card is not expanded after toggle, add 'unexpanded' class
+    if (!card.classList.contains('profile--expanded')) card.classList.toggle('profile--unexpanded');
+    // Else if card is expanded after toggle and still contains 'unexpanded' class, remove 'unexpanded'
+    else if (card.classList.contains('profile--expanded') && card.classList.contains('profile--unexpanded')) card.classList.toggle('profile--unexpanded');
+}
+
+
+
+
+
+// Modal Image Gallery
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+  }
+  
+
+  // Change style of navbar on scroll
+  window.onscroll = function() {myFunction()};
+  function myFunction() {
+      var navbar = document.getElementById("myNavbar");
+      var toplogo = document.getElementById("toplogo");
+      var menu = document.getElementById("menu");
+      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+          navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+          toplogo.style.opacity = "1";
+          menu.style.color ="black"
+      } else {
+          navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+          toplogo.style.opacity = "0";
+          menu.style.color ="white"
+      }
+  }
+  
+  // Used to toggle the menu on small screens when clicking on the menu button
+  function toggleFunction() {
+      var x = document.getElementById("navDemo");
+      if (x.className.indexOf("w3-show") == -1) {
+          x.className += " w3-show";
+      } else {
+          x.className = x.className.replace(" w3-show", "");
+      }
+  }
--- WebsiteEurocarga/mainsite/urls.py
+++ WebsiteEurocarga/mainsite/urls.py
@@ -17,6 +17,14 @@ urlpatterns = [
     path("tracking/", views.tracking, name="tracking"),
     path("offices/", views.offices, name="offices"),
     path("admin/", admin.site.urls),
+
+    path("en/", views.index_en, name="index_en"),
+    path("en/about/", views.about_en, name="about_en"),
+    path("en/team/", views.team_en, name="team_en"),
+    path("en/services/", views.services_en, name="services_en"),
+    path("en/pqrs/", views.pqrs_en, name="pqrs_en"),
+    path("en/offices/", views.offices_en, name="offices_en"),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
